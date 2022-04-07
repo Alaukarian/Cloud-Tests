087081428162130683dcb13239269839de8b6b92
Franco Parigi <Francogp93@gmail.com>
2022-04-07 13:37:30 +0200

    Pavlom

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Test Nordcloud Bartosz"),
+    body: JSON.stringify("Pavlom"),
   }
   return response 
 }
