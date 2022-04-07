f31b0a1e83a362b46ff1efb05122e249d32b75e5
Franco Parigi <Francogp93@gmail.com>
2022-04-07 11:37:55 +0200

    functional test1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("FOR FF, START"),
+    body: JSON.stringify("change, START"),
   }
   return response 
 }
